9e403c8aff70ee1e708fb23568f071045c8395ec
milly1110ning-dot <milly.1110ning@gmail.com>
2026-05-08 11:10:19 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Reflection.Metadata.Ecma335;
+
+public class Person
+{
+	public virtual string GetName()
+	{
+        return "Person";
+	}
+}
+public class  Student : Person
+{
+    public override string GetName()
+    {
+        return "Student";
+    }
+}
+
+public class People
+{
+    public string GetName()
+    {
+        return "People";
+    }
+}
+
+
+public class Teacher : People
+{
+    public new string GetName()
+    {
+        return "Teacher";
+    }
+}
+
---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Class_0508
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student s = new Student();
+
+            Console.WriteLine(s.GetName());
+
+            Person s2= new Student();
+
+            Console.WriteLine(s2.GetName());
+
+            Console.WriteLine(((Person)s2).GetName());
+
+            Console.WriteLine("***************");
+
+
+            Teacher t= new Teacher();
+
+            Console.WriteLine(t.GetName());
+
+            People t2= new Teacher();
+            Console.WriteLine(t2.GetName());
+
+            Console.WriteLine(((Teacher)t2).GetName());
+
+
+        }
+    }
+}
